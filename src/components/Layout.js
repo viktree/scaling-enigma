31a20862d27e9a5ce1d1bf0a78202bad78a390a6
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2018-12-08 17:09:16 -0500

    Add Logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,66 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
-import { rhythm, scale } from '../utils/typography'
-
 import Sidebar from './Sidebar'
 import './Layout.styl'
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props
-    const rootPath = `${__PATH_PREFIX__}/`
-    let header
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
-    return (
-      <div className="grid">
-        <Sidebar />
-        <div className="main">{children}</div>
-      </div>
-    )
-  }
+const Layout = ({ children }) => {
+  return (
+    <div className="grid">
+      <Sidebar />
+      <div className="main">{children}</div>
+    </div>
+  )
 }
 
 export default Layout
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import Toronto from './toronto'
+import Logo from './logo'
 import './Sidebar.styl'
 
 const Sidebar = props => {
   return (
     <div className="sidebar">
-      <div classNAme="sidebar-description">
-        This is Vikram's personal blog. He is a computer science student @UofT
-        who is currently working as a full-stack developer @ecobee.
+      <Toronto />
+      <div className="sidebar-container">
+        <Logo />
+        <div className="sidebar-description">
+          This is Vikram's personal blog. He is a computer science student @UofT
+          who is currently working as a full-stack developer @ecobee.
+        </div>
       </div>
     </div>
   )
