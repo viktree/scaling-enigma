6a7391f2c3dc2d0225bc770ce821ebceec71a25f
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2019-02-13 01:11:44 -0500

    Add form

---
+++ src/pages/thanks.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import { graphql } from 'gatsby'
+import { path } from 'ramda'
+
+import Layout from '../components/Layout'
+
+const ThanksPage = ({ data, location }) => {
+  const siteTitle = path(['site', 'siteMetadata', 'title'], data)
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <div>
+        <h1>Thank you!</h1>
+        <p>This is a custom thank you page for form submissions</p>
+      </div>
+    </Layout>
+  )
+}
+
+export default ThanksPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
