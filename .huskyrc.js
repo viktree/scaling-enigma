f3a76c4e1c4365a0e389e9a04534a346e10f2da4
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2019-03-08 00:00:24 -0500

    Auto add .nvmrc

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,7 +1,10 @@
 const tasks = arr => arr.join(' && ')
 
 const hooks = {
-  'pre-commit': tasks(['[ ! -e .nvmrc ]', 'node -v  > .nvmrc || :']),
+  'pre-commit': tasks([
+    '[ ! -e .nvmrc ] && node -v  > .nvmrc || :',
+    'git add .nvmrc',
+  ]),
 }
 
 module.exports = {
