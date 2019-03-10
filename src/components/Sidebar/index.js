f9680a38f0419f2351ff8d6fb5fe61dca931057e
Vikram Venkataramanan <vikram.venkataramanan@mail.utoronto.ca>
2019-03-10 12:26:53 -0400

    Remove sidebar links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { navigate } from 'gatsby-link'
 
 import Toronto from './toronto'
-import Logo from './logo'
+// import Logo from './logo'
 import './Sidebar.styl'
-import { isAbsolute } from 'path'
+// import { isAbsolute } from 'path'
 
 const SidebarLink = ({ id, name, path }) => {
   const handleClick = () => navigate(path)
@@ -15,59 +15,58 @@ const SidebarLink = ({ id, name, path }) => {
   )
 }
 
-const Sidebar = () => {
-  const sidebarItems = [
-    {
-      id: 'about',
-      name: 'About Me',
-      path: '/about/',
-    },
-    {
-      id: 'blog',
-      name: 'Blog',
-      path: '/blog/',
-    },
-    {
-      id: 'projects',
-      name: 'Projects',
-      path: '/projects/',
-    },
-    {
-      id: 'resume',
-      name: 'Resume',
-      path: '/resume/',
-    },
-    {
-      id: 'contact',
-      name: 'Get In Touch!',
-      path: '/contact/',
-    },
-  ]
-  return (
-    <div className="sidebar-container">
-      <Toronto />
-      <div className="sidebar-main">
+const sidebarItems = [
+  // {
+  //   id: 'about',
+  //   name: 'About Me',
+  //   path: '/about/',
+  // },
+  // {
+  //   id: 'blog',
+  //   name: 'Blog',
+  //   path: '/blog/',
+  // },
+  // {
+  //   id: 'projects',
+  //   name: 'Projects',
+  //   path: '/projects/',
+  // },
+  // {
+  //   id: 'resume',
+  //   name: 'Resume',
+  //   path: '/resume/',
+  // },
+  {
+    id: 'contact',
+    name: 'Get In Touch!',
+    path: '/contact/',
+  },
+]
+
+const Sidebar = () => (
+  <div className="sidebar-container">
+    <Toronto />
+    <div className="sidebar-main">
+      <br />
+      <div className="sidebar-description">
+        Welcome to this little portion of the internet that I am carving out for
+        myself!
         <br />
-        <div className="sidebar-description">
-          Welcome to this little portion of the internet that I am carving out
-          for myself!
-          <br />
-          <br />
-          My name is Vikram. I&#39;m computer science student{' '}
-          <a href="https://www.utoronto.ca/" className="sidebar-uoft-link">
-            @UofT
-          </a>{' '}
-          and an API developer{' '}
-          <a href="https://ecobee.com" className="sidebar-ecobee">
-            @ecobee
-          </a>
-          .
-        </div>
-        <br/>
-        {sidebarItems.map(SidebarLink)}
+        <br />
+        My name is Vikram. I&#39;m computer science student{' '}
+        <a href="https://www.utoronto.ca/" className="sidebar-uoft-link">
+          @UofT
+        </a>{' '}
+        and an API developer{' '}
+        <a href="https://ecobee.com" className="sidebar-ecobee">
+          @ecobee
+        </a>
+        .
       </div>
+      <br />
+      {sidebarItems.map(SidebarLink)}
     </div>
-  )
-}
+  </div>
+)
 
 export default Sidebar
